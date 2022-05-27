05d122847fe6e6916cb5b3e5ae31c09889902a61
edmundas3945 <74895998+edmundas3945@users.noreply.github.com>
2022-05-28 00:55:30 +0300

    fetching data from database to controler

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\models\User;
 
 class ProfilesController extends Controller
 {
-    //
+    public function index($user)
+    {
+        
+        $user = User::find($user);
+
+        return view('home', [
+            'user' => $user,
+
+        ]);
+    }
 }
