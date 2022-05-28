0b32da901bc7a0ca6c7dbe7dccc48daedf7467e1
edmundas3945 <74895998+edmundas3945@users.noreply.github.com>
2022-05-28 21:26:56 +0300

    new route to store post

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/p/create', [PostsController::class, 'create']);
+Route::post('/p', [PostsController::class, 'store']);
 
 Route::get('/profile/{user}', [ProfilesController::class, 'index'])->name('profile.show');
